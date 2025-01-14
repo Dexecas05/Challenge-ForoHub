3b68b12fd2a65f10485489d93a353ed29dedcdb4
Exequiel Castillo <execas.dec@gmail.com>
2025-01-14 18:55:51 -0300

    implementacion de las demas funcionalidades requeridas

--- forohub/src/main/java/com/daletguimel/forohub/domain/controller/TopicoController.java
+++ forohub/src/main/java/com/daletguimel/forohub/domain/controller/TopicoController.java
@@ -1,14 +1,14 @@
 package com.daletguimel.forohub.domain.controller;
 
-import com.daletguimel.forohub.domain.topico.CrearTopicoDTO;
-import com.daletguimel.forohub.domain.topico.Topico;
-import com.daletguimel.forohub.domain.topico.TopicoService;
+import com.daletguimel.forohub.domain.topico.*;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/topicos")
@@ -24,5 +24,35 @@ public class TopicoController {
     public ResponseEntity<Topico> registrarTopico(@RequestBody @Valid CrearTopicoDTO dto){
         Topico nuevoTopico = topicoService.registrarTopico(dto);
         return ResponseEntity.ok(nuevoTopico);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<TopicoDTO>> listarTopicos(
+            @PageableDefault(size = 10, sort = "fechaCreacion") Pageable pageable) {
+        return ResponseEntity.ok(topicoService.listarTopicos(pageable));
+    }
+
+    @GetMapping("/primeros10")
+    public ResponseEntity<List<TopicoDTO>> listarPrimeros10Topicos(){
+        return ResponseEntity.ok(topicoService.listarPrimeros10Topicos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TopicoDTO> obtenerTopicoPorId(@PathVariable Long id) {
+        TopicoDTO topicoDTO = topicoService.obtenerTopicoPorId(id);
+        return ResponseEntity.ok(topicoDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TopicoDTO> actualizarTopico(@PathVariable Long id,
+                                                      @RequestBody @Valid ActualizarTopicoDTO dto) {
+        TopicoDTO topicoActualizado = topicoService.actualizarTopico(id, dto);
+        return ResponseEntity.ok(topicoActualizado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarTopico(@PathVariable Long id) {
+        topicoService.eliminarTopico(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- forohub/src/main/java/com/daletguimel/forohub/domain/topico/TopicoService.java
+++ forohub/src/main/java/com/daletguimel/forohub/domain/topico/TopicoService.java
@@ -2,10 +2,15 @@ package com.daletguimel.forohub.domain.topico;
 
 import com.daletguimel.forohub.domain.usuario.Usuario;
 import com.daletguimel.forohub.domain.usuario.UsuarioRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class TopicoService {
@@ -31,5 +36,66 @@ public class TopicoService {
         Topico topico = new Topico(dto.titulo(), dto.mensaje(), LocalDateTime.now(),
                 "abierto", autor, dto.curso());
         return topicoRepository.save(topico);
+    }
+
+    //Listar Tópicos
+    public Page<TopicoDTO> listarTopicos(Pageable pageable){
+        return topicoRepository.findAll(pageable)
+                .map(this::convertirTopicoADTO);
+    }
+
+
+    //Listar primeros 10 tópicos según fecha de creación
+    public List<TopicoDTO> listarPrimeros10Topicos(){
+        return topicoRepository.findAll(Sort.by(Sort.Direction.ASC, "fechaCreacion")).stream()
+                .limit(10)
+                .map(this::convertirTopicoADTO)
+                .collect(Collectors.toList());
+    }
+
+    //Obtener detalles de tópico por id
+    public TopicoDTO obtenerTopicoPorId(Long id){
+        Topico topico = topicoRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Tópico no encontrado."));
+        return convertirTopicoADTO(topico);
+    }
+
+    //Actualizar tópico por id
+    public TopicoDTO actualizarTopico(Long id, ActualizarTopicoDTO dto){
+        Optional<Topico> topicoOptional = topicoRepository.findById(id);
+        if (topicoOptional.isPresent()){
+            Topico topico = topicoOptional.get();
+
+            Usuario autor = usuarioRepository.findById(dto.autorId())
+                    .orElseThrow(() -> new IllegalArgumentException("Autor no encontrado."));
+
+            topico.setTitulo(dto.titulo());
+            topico.setMensaje(dto.mensaje());
+            topico.setAutor(autor);
+            topico.setCurso(dto.curso());
+
+            topicoRepository.save(topico);
+            return convertirTopicoADTO(topico);
+        } else {
+            throw new IllegalArgumentException("Tópico no encontrado.");
+        }
+    }
+
+    //Eliminar tópico por id
+    public void eliminarTopico(Long id){
+        Optional<Topico> topicoOptional = topicoRepository.findById(id);
+
+        if (topicoOptional.isPresent()){
+            topicoRepository.deleteById(id);
+        } else {
+            throw new IllegalArgumentException("Tópico no encontrado.");
+        }
+    }
+
+    private TopicoDTO convertirTopicoADTO(Topico topico) {
+        return new TopicoDTO(
+                topico.getId(), topico.getTitulo(), topico.getMensaje(), topico.getFechaCreacion(),
+                topico.getStatus(), topico.getAutor().getId(), topico.getCurso()
+        );
     }
 }
